504cb38db3a35519b6543a623aec75c15a456e2d
josealberto.rodriguez.navarro.contractor <josealberto.rodriguez.navarro.contractor@bbva.com>
2023-07-12 11:00:35 -0600

    Unit Test Doumis

--- tests/bussineslogic/test_business_logic.py
+++ tests/bussineslogic/test_business_logic.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
 import pytest
+from pyspark.sql import Window
+from pyspark.sql.functions import count, col
+
 import exampleenginepythonqiyhbwvw.common.constants as c
 import exampleenginepythonqiyhbwvw.common.input as i
 import exampleenginepythonqiyhbwvw.common.output as o
@@ -9,12 +12,35 @@ import exampleenginepythonqiyhbwvw.common.output as o
 class TestBusinessLogic(TestCase):
 
     @pytest.fixture(autouse=True)
-    def spark_session(self, clients_df, business_logic):
+    def spark_session(self, clients_df, business_logic, contracts_dummy_df, clients_dummy_df, products_dummy_df):
         self.clients_df = clients_df
         self.business_logic = business_logic
+        self.clients_dummy_df = clients_dummy_df
+        self.contracts_dummy_df = contracts_dummy_df
+        self.products_dummy_df = products_dummy_df
 
     def test_filter_one(self):
-        output_df = self.business_logic.filter_one(self.clients_df)
-        self.assertEquals(output_df.filter(i.edad() < c.THIRTY_NUMBER).count(), 0)
-        self.assertEquals(output_df.filter(i.edad() > c.FIFTY_NUMBER).count(), 0)
-        self.assertEquals(output_df.filter(i.vip() != c.TRUE_VALUE).count(), 0)
+        self.clients_filtered_df = self.business_logic.filter_one(self.clients_df)
+        self.assertEquals(self.clients_filtered_df.filter(i.edad() < c.THIRTY_NUMBER).count(), 0)
+        self.assertEquals(self.clients_filtered_df.filter(i.edad() > c.FIFTY_NUMBER).count(), 0)
+        self.assertEquals(self.clients_filtered_df.filter(i.vip() != c.TRUE_VALUE).count(), 0)
+
+    def test_join_tables(self):
+        join_df = self.business_logic.join_tables(self.clients_dummy_df, self.contracts_dummy_df, self.products_dummy_df)
+        total_expected_columns = len(self.clients_dummy_df.columns) + \
+                                 len(self.contracts_dummy_df.columns) + \
+                                 len(self.products_dummy_df.columns) - 1
+        self.assertEquals(len(join_df.columns), total_expected_columns)
+
+    def test_filter_by_number_of_contracts(self):
+        output_df = self.business_logic.filter_by_number_of_contracts(self.clients_dummy_df)
+        validation_df = output_df.select(*output_df.columns, count(i.cod_client.name).over(Window.partitionBy(i.cod_client.name)).alias(c.COUNT_COLUMN))\
+                 .filter(col(c.COUNT_COLUMN) < c.THREE_NUMBER)\
+                 .drop(c.COUNT_COLUMN)
+        self.assertEquals(validation_df.count(), 0)
+        self.assertEquals(output_df.count(), 4)
+
+    def test_hash_column(self):
+        output_df = self.business_logic.hash_column(self.contracts_dummy_df)
+        self.assertEquals(len(output_df.columns), len(self.contracts_dummy_df.columns) + 1)
+        self.assertIn("hash", output_df.columns)
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,9 @@ import sys
 from pathlib import Path
 
 import pytest
+from pyspark import Row
 from pyspark.sql import SparkSession
-from dataproc_sdk import DatioPysparkSession
+from pyspark.sql.types import StructType, StructField, StringType
 
 from exampleenginepythonqiyhbwvw.bussineslogic.business_logic import BusinessLogic
 from exampleenginepythonqiyhbwvw.io.init_values import InitValues
@@ -42,15 +43,6 @@ def spark_test():
         .master("local[*]") \
         .getOrCreate()
 
-    """
-    spark = DatioPysparkSession() \
-        .getSparkSession().builder \
-        .appName("unittest_job") \
-        .config("spark.jars", sdk_path) \
-        .master("local[*]") \
-        .getOrCreate()
-    """
-
     yield spark
 
     spark.stop()
@@ -73,3 +65,47 @@ def clients_df(init_values):
         "clients_schema": "resources/schemas/clients_schema.json"
     }
     yield init_values.get_input_df(parameters_client, "clients_path", "clients_schema")
+
+
+@pytest.fixture(scope="session")
+def contracts_df(init_values):
+    parameters_contracts = {
+        "contracts_path": "resources/data/input/contracts.csv",
+        "contracts_schema": "resources/schemas/contracts_schema.json"
+    }
+    yield init_values.get_input_df(parameters_contracts, "contracts_path", "contracts_schema")
+
+
+@pytest.fixture(scope="session")
+def products_df(init_values):
+    parameters_products = {
+        "products_path": "resources/data/input/products.csv",
+        "products_schema": "resources/schemas/products_schema.json"
+    }
+    yield init_values.get_input_df(parameters_products, "products_path", "products_schema")
+
+
+@pytest.fixture(scope="session")
+def clients_dummy_df(spark_test):
+    data = [Row("111"), Row("111"), Row("111"), Row("111"), Row("123"), Row("123")]
+    schema = StructType([
+        StructField("cod_client", StringType())
+    ])
+    yield spark_test.createDataFrame(data, schema)
+
+@pytest.fixture(scope="session")
+def contracts_dummy_df(spark_test):
+    data = [Row("111", "aaa"), Row("123", "bbb")]
+    schema = StructType([
+        StructField("cod_titular", StringType()),
+        StructField("cod_producto", StringType())
+    ])
+    yield spark_test.createDataFrame(data, schema)
+
+@pytest.fixture(scope="session")
+def products_dummy_df(spark_test):
+    data = [Row("aaa"), Row("bbb")]
+    schema = StructType([
+        StructField("cod_producto", StringType())
+    ])
+    yield spark_test.createDataFrame(data, schema)
